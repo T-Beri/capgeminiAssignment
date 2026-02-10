392895a202702a53808f5d264db821109cb7db8a
Tithaye Beri <123239450+T-Beri@users.noreply.github.com>
2026-02-11 00:18:47 +0530

    feb 10 assignments

---
+++ feb10/activity 1/q2.java
@@ -0,0 +1,23 @@
+package SortingSearching;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class q2 {
+    public static void main(String[] args) {
+
+        Queue<String> queue=new LinkedList<>();
+
+        queue.add("Customer1");
+        queue.add("Customer2");
+        queue.add("Customer3");
+        queue.add("Customer4");
+        queue.add("Customer5");
+
+        System.out.println(queue);
+
+        queue.poll(); 
+        queue.poll(); 
+
+        System.out.println(queue);
+    }
+}
